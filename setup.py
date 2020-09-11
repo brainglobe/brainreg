c90aaa1abe78bb9122cc649b2c24c7b180eccfb9
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-09-11 10:45:40 +0100

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 
 setup(
     name="brainreg",
-    version="0.1.8",
+    version="0.1.9",
     description="Automated 3D brain registration",
     long_description=long_description,
     long_description_content_type="text/markdown",
