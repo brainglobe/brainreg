f85c42bf90484f4570978c6b193ca8dbab244482
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-10-02 17:11:37 +0100

    edit test

--- tests/tests/test_integration/test_registration.py
+++ tests/tests/test_integration/test_registration.py
@@ -60,9 +60,9 @@ def test_registration_niftyreg(tmpdir):
             "deformation_field_1.tiff",
             "deformation_field_2.tiff",
             # "downsampled.tiff",
-            "downsampled_brain.tiff",
-            "downsampled_standard.tiff",
-            "downsampled_standard_brain.tiff",
+            # "downsampled_brain.tiff",
+            # "downsampled_standard.tiff",
+            # "downsampled_standard_brain.tiff",
             "registered_atlas.tiff",
             "registered_hemispheres.tiff",
         ]
