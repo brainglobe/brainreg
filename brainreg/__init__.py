bd052db197be8cfbcdb241e1d76878b592438f22
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-10-12 11:46:06 +0100

    Bump version: 0.2.0 → 0.2.1

--- brainreg/__init__.py
+++ brainreg/__init__.py
@@ -1,3 +1,3 @@
 __author__ = "Adam Tyson"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 from . import *
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [
 
 setup(
     name="brainreg",
-    version="0.2.0",
+    version="0.2.1",
     description="Automated 3D brain registration",
     long_description=long_description,
     long_description_content_type="text/markdown",
