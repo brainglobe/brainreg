bd541816d02247a087463a39c56f3b845f7435d7
stephen <stephen.lenzi@gmail.com>
2021-07-05 13:08:09 +0100

    renames gui label from register to brainreg

--- brainreg_napari/plugins.py
+++ brainreg_napari/plugins.py
@@ -4,4 +4,4 @@ from napari_plugin_engine import napari_hook_implementation
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return [(register, {"name": "register"})]
+    return [(register, {"name": "brainreg"})]
