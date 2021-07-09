10363903f3b424e47ae810c68befe77c2957a08b
stephen <stephen.lenzi@gmail.com>
2021-07-09 15:25:59 +0100

    imports and groups segment plugin with register

--- brainreg_napari/plugins.py
+++ brainreg_napari/plugins.py
@@ -1,7 +1,11 @@
-from brainreg_napari.register import brainreg_register
 from napari_plugin_engine import napari_hook_implementation
+
+from brainreg_napari.register import brainreg_register
+from brainreg_segment.segment import SegmentationWidget
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return [(brainreg_register, {"name": "Atlas Registration"})]
+    return [(SegmentationWidget, {"name": "Manual segmentation"}),
+            (brainreg_register, {"name": "Atlas Registration"})]
+
