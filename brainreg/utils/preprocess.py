a275965d8ad88499fa1c315ca404f3585604f289
Iana Vasylieva <iana.vasylieva@gmail.com>
2022-04-22 09:48:13 -0400

    Add pre-processing functions for fMOST datasets

--- brainreg/utils/preprocess.py
+++ brainreg/utils/preprocess.py
@@ -1,19 +1,27 @@
 import numpy as np
 from tqdm import trange
 from skimage import morphology
-from scipy.ndimage import gaussian_filter
+from skimage.filters import threshold_triangle
+from scipy.ndimage import binary_fill_holes, gaussian_filter
+import cv2
 from imlib.image.scale import scale_and_convert_to_16_bits
 
 
-def filter_image(brain):
+def filter_image(brain, preprocessing_args=None):
     """
     Filter a 3D image to allow registration
     :return: The filtered brain
     :rtype: np.array
     """
     brain = brain.astype(np.float64, copy=False)
-    for i in trange(brain.shape[-1], desc="filtering", unit="plane"):
-        brain[..., i] = filter_plane(brain[..., i])
+    if preprocessing_args and preprocessing_args.preprocessing == "none":
+        pass
+    elif preprocessing_args and preprocessing_args.preprocessing == "fmost":
+        for i in trange(brain.shape[0], desc="filtering", unit="plane"):  # only coronal plane because of stripes
+            brain[i,:,:] = pre_process_fmost(brain[i,:,:])
+    else:  # default pre-processing
+        for i in trange(brain.shape[-1], desc="filtering", unit="plane"):
+            brain[..., i] = filter_plane(brain[..., i])
     brain = scale_and_convert_to_16_bits(brain)
     return brain
 
@@ -61,3 +69,120 @@ def pseudo_flatfield(img_plane, sigma=5):
     """
     filtered_img = gaussian_filter(img_plane, sigma)
     return img_plane / (filtered_img + 1)
+
+
+def ideal_notch_filter(fshift, points):
+    """
+
+    """
+    d0 = 5.0  # cutoff frequency
+    H, W = fshift.shape
+    u, v = np.ogrid[:H, :W]
+    for d in range(len(points)):
+        u0, v0 = points[d]
+        mask1 = (u - u0)**2 + (v - v0)**2 <= d0
+        mask2 = (u + u0)**2 + (v + v0)**2 <= d0
+        fshift[mask1] = 0
+        fshift[mask2] = 0
+    return fshift
+
+
+def remove_stripes(img_plane, stripes_direction="h"):
+    """
+
+    """
+    img_sum = np.sum(img_plane, axis=1)
+    fft_seq = np.abs(np.fft.rfft(img_sum))/img_sum.shape[0]
+    first_harmonic = np.argmax(fft_seq[10:]) + 10
+    H, W = img_plane.shape
+
+    # do 2D fft
+    img_fft = np.fft.fft2(img_plane.astype(np.float32)) / (W * H)
+    # shift fft to get maximum at the center
+    img_fft = np.fft.fftshift(img_fft)
+
+    # filter shifted fft
+    points = []
+    image_center = (H // 2, W // 2)
+    if stripes_direction == "h":
+        # compute points on vertical axis of symmetry
+        for point_ind in range(1, image_center[0] // first_harmonic):
+            points.extend([
+                [image_center[0] + point_ind * first_harmonic, image_center[1]],
+                [image_center[0] - point_ind * first_harmonic, image_center[1]]
+            ])
+    elif stripes_direction == "v":
+        # compute points on horizontal axis of symmetry
+        for point_ind in range(1, image_center[1] // first_harmonic):
+            points.extend([
+                [image_center[0], image_center[1] + point_ind * first_harmonic],
+                [image_center[0], image_center[1] - point_ind * first_harmonic]
+            ])
+    else:
+        raise NotImplemented("Can only automatically remove vertical or horizontal stripes")
+    points = np.asarray(points)
+
+    filtered_fft_shift = ideal_notch_filter(img_fft, points)
+
+    # unshift
+    img_fft = np.fft.ifftshift(filtered_fft_shift)
+    # do inverse fft
+    out_ifft = np.fft.ifft2(img_fft)
+    img_plane = (np.abs(out_ifft) * W * H)
+    return img_plane
+
+
+def denoise_fft(img_plane):
+    """
+    Apply circular mask to the image in FFT domain.
+    """
+    H, W = img_plane.shape
+    img_fft = np.fft.fft2(img_plane)/(W * H)
+
+    img_fft = np.fft.fftshift(img_fft)
+
+    center = [H//2, W//2]
+    r = 200  # TODO hardcoded, change to % of smallest dimension
+    x, y = np.ogrid[:H, :W]
+    mask_area = (x - center[0])**2 + (y - center[1])**2 >= r**2
+    img_fft[mask_area] = 0
+
+    img_fft = np.fft.ifftshift(img_fft)
+    out_ifft = np.fft.ifft2(img_fft)
+
+    img_plane = (np.real(out_ifft) * W * H)
+    return img_plane
+
+
+def subtract_background(img_plane):
+    """
+    Subtract background from downsampled_standard.tif image, to compute overlap and nmi with atlas.
+
+    Mask is computed from denoised image with low cutoff frequency
+    (blured signigicantly) for better thresholding.
+    """
+    img_lf = denoise_fft(img_plane)
+    try:
+        thr = threshold_triangle(img_lf)
+    except:  # attempt to get argmax of an empty sequence
+        return np.zeros_like(img_plane).astype(np.uint16)
+
+    mask = (img_lf > thr).astype(np.uint8)
+    mask = (binary_fill_holes(mask)).astype(np.uint8)
+    kernel = morphology.disk(3)
+    mask = (morphology.opening(mask, kernel)).astype(np.uint8)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
+    sizes = stats[1:, -1]
+    nb_components -= 1
+    for i in range(nb_components):
+        if sizes[i] < 12000:  # TODO hardcoded
+            mask[output == i + 1] = np.abs(mask[output == i + 1] - 1)
+
+    return mask
+
+
+def pre_process_fmost(img_plane):
+    foreground_mask = subtract_background(img_plane)
+    img_plane = remove_stripes(img_plane)
+    img_plane[foreground_mask == 0] = 0
+    return img_plane
