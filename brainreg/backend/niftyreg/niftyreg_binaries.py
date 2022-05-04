16125ab0efd981294ab280b43da347eb6b7e1a3d
David Stansby <dstansby@gmail.com>
2022-05-04 18:38:41 +0100

    Fix niftyreg binary finding

--- brainreg/backend/niftyreg/niftyreg_binaries.py
+++ brainreg/backend/niftyreg/niftyreg_binaries.py
@@ -1,25 +1,11 @@
 import os
-import platform
 from pkg_resources import resource_filename
 
 
-__os_folder_names = {"Linux": "linux_x64", "Darwin": "osX", "Windows": "win64"}
-
-try:
-    os_folder_name = __os_folder_names[platform.system()]
-except KeyError:
-    raise ValueError(
-        "Platform {} is not recognised as a valid platform. "
-        "Valid platforms are : {}".format(
-            platform.system(), __os_folder_names.keys()
-        )
-    )
-
-
 def get_binary(binaries_folder, program_name):
-    path = os.path.join(binaries_folder, os_folder_name, program_name)
+    path = os.path.join(binaries_folder, program_name)
     return path
 
 
 def get_niftyreg_binaries():
-    return resource_filename("brainreg", "bin/nifty_reg")
+    return resource_filename("brainreg", "nifty_reg/bin")
