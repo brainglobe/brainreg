12bec44fb989f9902a624d5c6f3358e9c77e9eca
David Stansby <dstansby@gmail.com>
2022-06-20 11:48:13 +0100

    Add a test for sample data

--- brainreg_napari/tests/test_brainreg_napari.py
+++ brainreg_napari/tests/test_brainreg_napari.py
@@ -1,4 +1,5 @@
 from brainreg_napari.register import brainreg_register
+from brainreg_napari.sample_data import load_test_brain
 
 
 def test_add_detect_widget(make_napari_viewer):
@@ -8,3 +9,13 @@ def test_add_detect_widget(make_napari_viewer):
     viewer = make_napari_viewer()
     widget = brainreg_register()
     viewer.window.add_dock_widget(widget)
+
+
+def test_get_sample_data():
+    """
+    Check that fetching the test brain works.
+    """
+    layer_data_list = load_test_brain()
+    layer_data = layer_data_list[0]
+    assert layer_data[0].shape == (270, 193, 271)
+    assert layer_data[2] == "image"
