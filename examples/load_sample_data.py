1ee329fe4625b9a9005c9aad097b35e275ac3a4f
David Stansby <dstansby@gmail.com>
2022-06-21 11:14:55 +0100

    Set sensible contrast limits

--- examples/load_sample_data.py
+++ examples/load_sample_data.py
@@ -7,26 +7,31 @@ This example:
 - loads the brainreg-napari registration plugin
 - opens the napari viewer
 """
+import numpy as np
 import napari
 from napari.layers import Layer
 
 from brainreg_napari.sample_data import load_test_brain
 
 viewer = napari.Viewer()
+
+layer_data = load_test_brain()[0]
+# Set sensible contrast limits for viewing the brain
+layer_data[1]['contrast_limits'] = np.percentile(layer_data[0], [0, 99.5])
+# Add data to napari
+viewer.add_layer(Layer.create(*layer_data))
+
 # Open plugin
 _, brainreg_widget = viewer.window.add_plugin_dock_widget(
     plugin_name="brainreg-napari", widget_name="Atlas Registration"
 )
-# Add sample data layers
-for layer_data in load_test_brain():
-    viewer.add_layer(Layer.create(*layer_data))
-
 # Set brainreg-napari plugin settings from sample data metadata
 metadata = layer_data[1]["metadata"]
 brainreg_widget.data_orientation.value = metadata["data_orientation"]
 for i, dim in enumerate(["z", "y", "x"]):
     pixel_widget = getattr(brainreg_widget, f"{dim}_pixel_um")
     pixel_widget.value = metadata["voxel_size"][i]
+
 
 if __name__ == "__main__":
     # The napari event loop needs to be run under here to allow the window
