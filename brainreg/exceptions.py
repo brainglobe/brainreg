2a629a88feeeb0522d2fae1449439c2068eed90c
JoeZiminski <Joseph.j.ziminski@gmail.com>
2022-10-26 09:23:39 +0000

    fix linting on exceptions

--- brainreg/exceptions.py
+++ brainreg/exceptions.py
@@ -5,34 +5,37 @@ from os.path import isdir
 
 class LoadFileException(Exception):
     """
-    Custom exception class for errors found loading image with imio.load_any (in main.py).
+    Custom exception class for errors found loading image with
+    imio.load_any (in main.py).
 
-    If the passed target brain directory contains only a single .tiff, alert the user the
-    full filepath including the filename. Otherwise, alert the use there was an issue loading
-    the file and including the full traceback
-
-    TODO: make target_brain_path a class attribute, after determine whether to move
-          path_is_folder_with_one_tiff() and search_dir_for_image_files() to utils class.
+    If the passed target brain directory contains only a single
+    .tiff, alert the user the full filepath including the filename.
+    Otherwise, alert the use there was an issue loading the file and
+    including the full traceback
     """
 
     def __init__(self, target_brain_path, base_error):
 
         if self.path_is_folder_with_one_tiff(target_brain_path):
             super().__init__(
-                "Attempted to load directory containing single .tiff file. For 3D tiff, pass the full path including filename"
+                "Attempted to load directory containing single .tiff file. "
+                "For 3D tiff, pass the full path including filename"
             )
         else:
             origional_traceback = "".join(
                 tb.format_tb(base_error.__traceback__) + [base_error.__str__()]
             )
             super().__init__(
-                f"{origional_traceback}\nFile at {target_brain_path} failed to load. Ensure all image files contain the same number of pixels. Full traceback above."
+                f"{origional_traceback}\nFile at {target_brain_path} "
+                f"failed to load. Ensure all image files contain the "
+                f"same number of pixels. Full traceback above."
             )
 
     def path_is_folder_with_one_tiff(self, target_brain_path):
         """
-        Check that the target dir contains only 1 TIFF. In this case, the path to the
-        .tiff should be passed not the directory.
+        Check that the target dir contains only 1 TIFF.
+        In this case, the path to the .tiff should be
+        passed not the directory.
         """
         if isdir(target_brain_path):
             image_files = self.search_dir_for_image_files(target_brain_path)
@@ -45,8 +48,6 @@ class LoadFileException(Exception):
         """
         Search a folder for any images that can be loaded by brainreg
 
-        TODO: - add this to a utils module
-              - set a config somewhere that holds all permitted image types to load. This is also needed at niftireg/run
         """
         images_in_dir = [
             file
