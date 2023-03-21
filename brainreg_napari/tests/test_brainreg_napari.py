30355b0a91115aa5693a24cc83b84652a162a523
Adam Tyson <code@adamltyson.com>
2023-03-21 12:15:09 +0000

    Add orientation check tests

--- brainreg_napari/tests/test_brainreg_napari.py
+++ brainreg_napari/tests/test_brainreg_napari.py
@@ -1,5 +1,6 @@
 import napari
 import pytest
+from bg_atlasapi import BrainGlobeAtlas
 
 from brainreg_napari.register import (
     add_registered_image_layers,
@@ -75,6 +76,79 @@ def test_workflow(make_napari_viewer, tmp_path):
     boundaries = viewer.layers[2]
     assert isinstance(boundaries, napari.layers.Image)
     assert boundaries.name == "Boundaries"
+
+
+def test_orientation_check(
+    make_napari_viewer, tmp_path, atlas_choice="allen_mouse_50um"
+):
+    """
+    Test that the check orientation function works
+    """
+    viewer = make_napari_viewer()
+
+    # Load widget
+    _, widget = viewer.window.add_plugin_dock_widget(
+        plugin_name="brainreg-napari", widget_name="Atlas Registration"
+    )
+
+    # Set a specific atlas
+    # Should be a better way of doing this
+    for choice in widget.atlas_key.choices:
+        if choice.name == atlas_choice:
+            widget.atlas_key.value = choice
+            break
+
+    assert widget.atlas_key.value.name == atlas_choice
+
+    widget.check_orientation_button.clicked()
+    assert len(viewer.layers) == 0
+
+    # Load sample data
+    added_layers = viewer.open_sample("brainreg-napari", "sample")
+    brain_layer = added_layers[0]
+
+    assert len(viewer.layers) == 1
+
+    atlas = BrainGlobeAtlas(atlas_choice)
+    # click check orientation button and check output
+    run_and_check_orientation_check(widget, viewer, brain_layer, atlas)
+
+    # run again and check previous output was deleted properly
+    run_and_check_orientation_check(widget, viewer, brain_layer, atlas)
+
+
+def run_and_check_orientation_check(widget, viewer, brain_layer, atlas):
+    widget.check_orientation_button.clicked()
+    check_orientation_output(viewer, brain_layer, atlas)
+
+
+def check_orientation_output(viewer, brain_layer, atlas):
+    # 1 for the loaded data, and three each for the
+    # views of two images (data & atlas)
+    assert len(viewer.layers) == 7
+    assert brain_layer.visible is False
+    for i in range(1, 7):
+        layer = viewer.layers[i]
+        assert isinstance(layer, napari.layers.Image)
+        assert layer.visible is True
+        assert layer.ndim == 2
+
+    assert viewer.layers[4].data.shape == (
+        brain_layer.data.shape[1],
+        brain_layer.data.shape[2],
+    )
+    assert viewer.layers[5].data.shape == (
+        brain_layer.data.shape[0],
+        brain_layer.data.shape[2],
+    )
+    assert viewer.layers[6].data.shape == (
+        brain_layer.data.shape[0],
+        brain_layer.data.shape[1],
+    )
+
+    assert viewer.layers[1].data.shape == (atlas.shape[1], atlas.shape[2])
+    assert viewer.layers[2].data.shape == (atlas.shape[0], atlas.shape[2])
+    assert viewer.layers[3].data.shape == (atlas.shape[0], atlas.shape[1])
 
 
 def test_add_layers_errors(tmp_path, make_napari_viewer):
