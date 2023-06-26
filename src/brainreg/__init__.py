f827ebc4423cb8ee5bda1ccca85778bb13411b5f
Adam Tyson <code@adamltyson.com>
2023-06-26 15:34:31 +0100

    Don't hardcode version

--- src/brainreg/__init__.py
+++ src/brainreg/__init__.py
@@ -1,3 +1,10 @@
-__author__ = "Adam Tyson"
-__version__ = "0.4.0"
+from importlib.metadata import PackageNotFoundError, version
 from . import *
+
+try:
+    __version__ = version("brainreg")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+__author__ = "Adam Tyson"
