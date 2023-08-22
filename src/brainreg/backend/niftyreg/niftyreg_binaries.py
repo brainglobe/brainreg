091120d6ff32bd8f749c6f4e6278a8c883064a8e
Will Graham <32364977+willGraham01@users.noreply.github.com>
2023-08-22 13:45:27 +0100

    Find conda-installed binaries on Windows (#134)
    * Tmate session for windows debugging
    
    * Add Windows conda-path differences
    
    * Add a test checking for CONDA-installed binaries
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Actually enable the package to be built
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/brainreg/backend/niftyreg/niftyreg_binaries.py
+++ src/brainreg/backend/niftyreg/niftyreg_binaries.py
@@ -6,30 +6,52 @@ from typing import Optional
 __os_folder_names = {"Linux": "linux_x64", "Darwin": "osX", "Windows": "win64"}
 
 try:
-    os_folder_name = __os_folder_names[platform.system()]
+    os_system_name = platform.system()
+    os_folder_name = __os_folder_names[os_system_name]
 except KeyError:
     raise ValueError(
         f"Platform {platform.system()} is not recognised as a valid platform. "
         f"Valid platforms are : {__os_folder_names.keys()}"
     )
 
+_IS_WINDOWS_OS = os_system_name == "Windows"
 
-def conda_install_path() -> Optional[Path]:
+packaged_binaries_folder = (
+    Path(__file__).parent.parent.parent / "bin" / "nifty_reg"
+)
+
+
+def conda_niftyreg_path() -> Optional[Path]:
     """
     If a conda install of niftyreg is available, return the directory
     containing the niftyreg binaries.
     """
-    if "CONDA_PREFIX" not in os.environ:
-        return None
+    if "CONDA_PREFIX" in os.environ:
+        conda_prefix = Path(os.environ["CONDA_PREFIX"])
 
-    bin_path = Path(os.environ["CONDA_PREFIX"]) / "bin"
-    if (bin_path / "reg_aladin").exists():
-        return bin_path
+        if platform.system() == "Windows":
+            # Install prefix into conda environments on Windows
+            # is CONDA_PREFIX / Library / bin
+            bin_path = conda_prefix / "Library" / "bin"
 
+            # Determine if the binaries are present
+            # Binaries MUST have .exe appended to them in this case
+            if (bin_path / "reg_aladin.exe").exists():
+                return bin_path
+        else:
+            # On MacOS and Linux, binaries are placed into
+            # CONDA_PREFIX / bin
+            bin_path = conda_prefix / "bin"
+
+            # Determine if the binaries are present
+            if (bin_path / "reg_aladin").exists():
+                return bin_path
+    # If the binaries are not installed into the conda environment,
+    # or there is no conda environment, return None as a fail-case
     return None
 
 
-_CONDA_INSTALL_PATH = conda_install_path()
+_CONDA_NIFTYREG_BINARY_PATH = conda_niftyreg_path()
 
 
 def get_binary(program_name: str) -> Path:
@@ -39,10 +61,14 @@ def get_binary(program_name: str) -> Path:
     If niftyreg is installed via conda, use those binaries, otherwise fall
     back on bundled binaries.
     """
-    if _CONDA_INSTALL_PATH is not None:
-        return _CONDA_INSTALL_PATH / program_name
+    if _CONDA_NIFTYREG_BINARY_PATH is not None:
+        bin_path = _CONDA_NIFTYREG_BINARY_PATH / program_name
     else:
-        binaries_folder = (
-            Path(__file__).parent.parent.parent / "bin" / "nifty_reg"
-        )
-        return Path(binaries_folder) / os_folder_name / program_name
+        bin_path = packaged_binaries_folder / os_folder_name / program_name
+
+    # Append exe label to Windows executables
+    # It looks like subprocess is actually able to cope without the .exe
+    # appended, but just to be safe we'll include it on Windows OS calls
+    if _IS_WINDOWS_OS:
+        bin_path = bin_path.parent / f"{bin_path.stem}.exe"
+    return bin_path
---
+++ tests/tests/test_backend/test_niftyreg_detection.py
@@ -0,0 +1,83 @@
+import os
+from pathlib import Path
+from typing import Tuple
+
+from brainreg.backend.niftyreg.niftyreg_binaries import (
+    _CONDA_NIFTYREG_BINARY_PATH,
+    get_binary,
+    packaged_binaries_folder,
+)
+
+
+def packaged_binaries_are_used() -> Tuple[bool, Path]:
+    """
+    Return a (bool, Path) tuple which has the values:
+        boolean:
+            TRUE if the get_binary function points to one of the niftyreg
+            binaries that are included with the package.
+            FALSE otherwise.
+        Path:
+            The path to the folder containing the niftyreg binaries that
+            were found.
+    """
+    using_binary = get_binary("reg_aladin")
+    return (
+        packaged_binaries_folder in using_binary.parents,
+        using_binary.parent,
+    )
+
+
+def test_conda_with_niftyreg():
+    """
+    Check the following:
+    - If we are not in a conda environment, the packaged niftyreg binaries
+    are used by the backend
+    - If we are in a conda environment and niftyreg is not conda-installed,
+    the packaged niftyreg binaries are used by the backend
+    - If we are in a conda environment and niftyreg IS conda-installed,
+    the conda-installed binaries are used by the backend.
+
+    We need the assert_msg's to get round mypy thinking we're passing tuples
+    to assert (since Tuples always evaluate to true, and mypy wants to split
+    long code lines).
+    """
+    if "CONDA_PREFIX" not in os.environ:
+        # We are not in a conda envrionment
+        # _CONDA_NIFTYREG_BINARY_PATH should be none
+        assert_msg = (
+            "Not in a conda environment but "
+            "_CONDA_NIFTYREG_BINARY_PATH is non-None: "
+            f"{_CONDA_NIFTYREG_BINARY_PATH}"
+        )
+        assert _CONDA_NIFTYREG_BINARY_PATH is None, assert_msg
+
+        using_packaged_binaries, bin_folder = packaged_binaries_are_used()
+        assert_msg = (
+            "There is no CONDA_PREFIX, "
+            f"but non-packaged binaries in {bin_folder} "
+            "are being used!"
+        )
+        assert using_packaged_binaries, assert_msg
+    else:
+        # We are in a conda environment.
+        # Either this environment does not have niftyreg installed, or it does.
+        if _CONDA_NIFTYREG_BINARY_PATH is None:
+            # Apparently niftyreg is not installed in this environment
+            # Assert the located binaries are the packaged ones
+            using_packaged_binaries, bin_folder = packaged_binaries_are_used()
+            assert_msg = (
+                "Conda environment without niftyreg installed exists, "
+                f"but non-packaged binaries in {bin_folder} "
+                "are being used."
+            )
+            assert using_packaged_binaries, assert_msg
+        else:
+            # We are in a conda environment that _has_ niftyreg conda-installed
+            # Assert that we are using the CONDA-installed binaries
+            using_packaged_binaries, bin_folder = packaged_binaries_are_used()
+            assert_msg = (
+                "Packaged binaries are being used, "
+                "despite niftyreg appearing to be installed by CONDA "
+                f" at {_CONDA_NIFTYREG_BINARY_PATH}"
+            )
+            assert not using_packaged_binaries, assert_msg
