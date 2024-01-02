7efddfc62a5a1e20e996168fb590e9b527e03b0f
Adam Tyson <code@adamltyson.com>
2024-01-02 15:10:05 +0000

    Fix tests (#170)
    * Remove unnecessary pytest options
    
    * Update dependency imports
    
    * Add test for get_available_atlases
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update tests/tests/test_unit/test_plugin/test_register.py
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>

---
+++ tests/tests/test_unit/test_plugin/test_register.py
@@ -0,0 +1,20 @@
+from brainreg.napari.register import get_available_atlases
+
+atlas_name = "allen_mouse_50um"
+
+
+def test_get_available_atlases():
+    atlases = get_available_atlases()
+
+    # arbitrary selection of atlases
+    expected_atlases = [
+        "allen_mouse_10um",
+        "allen_mouse_25um",
+        "allen_mouse_50um",
+        "mpin_zfish_1um",
+    ]
+
+    for a in expected_atlases:
+        assert (
+            a in atlases.keys()
+        ), f"{a} is not in the list of expected atlases"
